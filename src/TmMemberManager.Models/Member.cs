147651d5f33ae8eb11c63915b786ab1f8e7507b1
Tony Bjerstedt <tony@BjerstedtTechnologies.com>
2019-10-21 23:08:33 -0500

    Add models and empty tests

---
+++ src/TmMemberManager.Models/Member.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TmMemberManager.Models
+{
+    /// <summary>
+    /// Public information available for a member
+    /// </summary>
+    public class Member
+    {
+        /// <summary>
+        /// Official member number on Toastmaster's roster.
+        /// </summary>
+        public int TmMemberNumber { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email {get;set;}
+
+        /// <summary>
+        /// Indicates that member is current member and listed on official roster.
+        /// </summary>
+        public bool IsCurrent{get;set;}
+
+        /// <summary>
+        /// Indicates that member is currently active. Members may stop coming, but
+        /// remain on club roster.
+        /// </summary>
+        public bool IsActive {get;set;}
+
+        /// <summary>
+        /// Indicates that member is paid uo for current period.
+        /// </summary>
+        public bool IsPaid{get;set;}
+    }
+}
---
+++ src/TmMemberManager.Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TmMemberManager.Models
+{
+    /// <summary>
+    /// Roles that a member can perform at a meeting
+    /// </summary>
+    public class Role
+    {
+        public string RoleCode { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+        public string Group { get; set; }
+        public int GroupRank { get; set; }
+    }
+}
